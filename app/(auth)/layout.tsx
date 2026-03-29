ebff829aa9473a505d41ecaad6a0aa10d54f6aca
BOHDAN BEREST <berestbodi@gmail.com>
2026-03-29 12:54:01 +0300

    feat: implement auth layout and shared authentication components #16

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,44 @@
+import { Metadata } from 'next';
+import AuthHeader from '@/components/Auth/AuthHeader/AuthHeader';
+import AuthFooter from '@/components/Auth/AuthFooter/AuthFooter';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Природні Мандри',
+    default: 'Природні Мандри',
+  },
+  description:
+    'Приєднуйтесь до спільноти мандрівників "Природні Мандри". Увійдіть або зареєструйтесь, щоб ділитися своїми історіями.',
+  openGraph: {
+    title: 'Природні Мандри — Авторизація',
+    description:
+      'Мінімалістичний інтерфейс для входу та реєстрації у спільноті мандрівників.',
+    type: 'website',
+    images: [
+      {
+        url: '/Hero.webp',
+        width: 1200,
+        height: 630,
+        alt: 'Природні Мандри Авторизація',
+      },
+    ],
+  },
+};
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <AuthHeader />
+
+      <main>
+        <div>{children}</div>
+      </main>
+
+      <AuthFooter />
+    </>
+  );
+}
