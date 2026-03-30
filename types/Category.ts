5a81a8c41080eb20eab0763c8f6eb0092239d93b
Dmytro <dimalifecell2019@gmail.com>
2026-03-30 18:12:45 +0300

    feat: add Category interface and SortCategory type for story categorization

---
+++ types/Category.ts
@@ -0,0 +1,12 @@
+export interface Category {
+  _id: string;
+}
+
+export type SortCategory = [
+  'Всі статті',
+  'Маршрути',
+  'Еко-поради',
+  'Природа',
+  'Культура',
+  'Локальні продукти',
+];
