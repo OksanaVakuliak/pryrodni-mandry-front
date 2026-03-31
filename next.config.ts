2260555e59ba38a669453d7fd9508854744cae0f
BOHDAN BEREST <berestbodi@gmail.com>
2026-03-31 12:59:04 +0300

    fix: delete next config extra options

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ftp.goit.study',
-        port: '',
-        pathname: '/img/harmoniq/**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
