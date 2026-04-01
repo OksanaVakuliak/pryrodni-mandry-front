77c213b74ff5f2cfa7eb780a5611dc452087de7d
Dmytro <dimalifecell2019@gmail.com>
2026-04-01 13:15:40 +0300

    feat: implement travelers API route and homepage components with data fetching

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -1,36 +1,31 @@
 import instance from './api';
 import { Story } from '@/types/Stories';
 import { Category } from '@/types/Category';
-import { Traveller, TravellersResponse } from '@/types/traveller';
+import { Traveler, TravelersResponse } from '@/types/traveler';
 
 export const clientApi = {
   stories: {
-    getAll: async () => {
+    getAll: async (): Promise<Story[]> => {
       const { data } = await instance.get<Story[]>('/stories');
       return data;
     },
+    getPopular: async (): Promise<Story[]> => {
+      const res = await instance.get<Story[]>('/stories/popular');
+      return res.data;
+    },
   },
   categories: {
-    getAll: async () => {
+    getAll: async (): Promise<Category[]> => {
       const { data } = await instance.get<Category[]>('/categories');
       return data;
     },
   },
-};
-
-
-export const getPopularStories = async (): Promise<Story[]> => {
-  const res = await instance.get<Story[]>('/stories/popular');
-  return res.data;
-};
-
-export const getTravellers = async (
-  perPage: number = 10,
-  page: number = 1,
-): Promise<Traveller[]> => {
-  const { data } = await instance.get<TravellersResponse>('/travellers', {
-    params: { perPage, page },
-  });
-
-  return data.data.users;
+  travelers: {
+    getAll: async (perPage: number = 10, page: number = 1): Promise<Traveler[]> => {
+      const { data } = await instance.get<TravelersResponse>('/travelers', {
+        params: { perPage, page },
+      });
+      return data.data.users;
+    },
+  },
 };
