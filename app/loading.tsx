cecebd583cfc1381102313f85bc64fb2ee4466b4
Dmytro <dimalifecell2019@gmail.com>
2026-04-04 00:18:02 +0300

    fix: delete Skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,17 +1,5 @@
-import { Skeleton } from '@/components/ui/Skeleton/Skeleton';
 import { Loader } from '@/components/ui/Loader/Loader';
 
 export default function Page() {
-  return (
-    <div style={{ padding: '20px', position: 'relative' }}>
-      <Loader />
-      <Skeleton width="100%" height="40px" className="mb-4" />
-      <Skeleton width="80%" height="20px" className="mb-2" />
-      <Skeleton width="60%" height="20px" className="mb-4" />
-      <Skeleton width="100%" height="200px" className="mb-4" />
-      <Skeleton width="100%" height="20px" className="mb-2" />
-      <Skeleton width="100%" height="20px" className="mb-2" />
-      <Skeleton width="100%" height="20px" />
-    </div>
-  );
+  return <Loader />;
 }
