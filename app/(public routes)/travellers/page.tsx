426cbad51f30741b64c65155144ec150c01fe99b
Tenebris An <y2222672@gmail.com>
2026-04-03 16:52:13 +0300

    feat: implement Travellers list with pagination and dynamic loading

--- app/(public routes)/travellers/page.tsx
+++ app/(public routes)/travellers/page.tsx
@@ -1,3 +1,20 @@
-export default function AllTravellersPage() {
-  return <></>;
-}
+import { Metadata } from 'next';
+import { PageTitle } from '@/components/ui/PageTitle/PageTitle';
+import TravellersList from '@/components/TravellersPage/TravellersList/TravellersList';
+
+export const metadata: Metadata = {
+  title: 'Мандрівники',
+  description: 'Список свідомих мандрівників нашої спільноти',
+};
+
+const TravellersPage = () => {
+  return (
+    <main className="container">
+      <PageTitle tag="h1" className="visually-hidden">
+        Мандрівники
+      </PageTitle>
+      <TravellersList />
+    </main>
+  );
+};
+export default TravellersPage;
