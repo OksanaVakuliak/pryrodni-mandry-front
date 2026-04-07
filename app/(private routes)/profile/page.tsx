6441d12ede0389ba0be58e6845c0bb534c11b99e
PolinaGr <polinasoukh@gmail.com>
2026-04-07 11:19:23 +0300

    fix metadata

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -3,31 +3,6 @@
 import { useAuthStore } from '@/lib/store/useAuthStore';
 import TravellerInfo from '@/components/ui/TravellerInfo/TravellerInfo';
 import ProfileTabs from '@/components/ui/ProfileTabs/ProfileTabs';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Мій профіль',
-  description:
-    'Ваш профіль з власними та збереженими історіями подорожей, пригод та відкриттів у спільноті Природні мандри.',
-
-  openGraph: {
-    title: 'Мій профіль — Природні мандри',
-    description:
-      'Ваш профіль з власними та збереженими історіями подорожей, пригод та відкриттів у спільноті Природні мандри.',
-    url: 'https://your-site.com/profile',
-    siteName: 'Природні мандри',
-    images: [
-      {
-        url: '/Image/Hero.webp',
-        width: 1200,
-        height: 630,
-        alt: 'Природні мандри — створення історії',
-      },
-    ],
-    locale: 'uk_UA',
-    type: 'website',
-  },
-};
 
 export default function ProfilePage() {
   const user = useAuthStore((state) => state.user);
