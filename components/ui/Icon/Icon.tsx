903e0ac3b25f80582f6f908423eb86d17fc41825
Oksana Vakuliak <oksanabagrova19@gmail.com>
2026-04-07 10:25:12 +0200

    fix: correct icon sprite path in Icon component

--- components/ui/Icon/Icon.tsx
+++ components/ui/Icon/Icon.tsx
@@ -21,7 +21,7 @@ export const Icon = ({
       aria-hidden="true"
       {...props}
     >
-      <use href={`./Icons/sprite.svg#${name}`} />
+      <use href={`/Icons/sprite.svg#${name}`} />
     </svg>
   );
 };
