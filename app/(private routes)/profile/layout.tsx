db3ef1dfbab207630ae8697d3f86020888ed0c0e
PolinaGr <polinasoukh@gmail.com>
2026-04-07 13:00:58 +0300

    fix: render userInfo after refresh

--- app/(private routes)/profile/layout.tsx
+++ app/(private routes)/profile/layout.tsx
@@ -2,26 +2,36 @@
 import { ReactNode } from 'react';
 import css from './ProfileLayout.module.css';
 import { usePathname } from 'next/navigation';
+import { useAuthStore } from '@/lib/store/useAuthStore';
+import TravellerInfo from '@/components/ui/TravellerInfo/TravellerInfo';
+import ProfileTabs from '@/components/ui/ProfileTabs/ProfileTabs';
 
 interface ProfileLayoutProps {
-  children: ReactNode;
   saved: ReactNode;
   my: ReactNode;
 }
 
-export default function ProfileLayout({
-  children,
-  saved,
-  my,
-}: ProfileLayoutProps) {
+export default function ProfileLayout({ saved, my }: ProfileLayoutProps) {
   const pathname = usePathname();
-
+  const user = useAuthStore((state) => state.user);
   const isMyStories = pathname === '/profile/my';
 
   return (
     <div className={css.layoutContainer}>
       <div className={css.innerWrapper}>
-        <section className={css.userInfoSection}>{children}</section>
+        <section className={css.userInfoSection}>
+          {' '}
+          {user && (
+            <>
+              <TravellerInfo
+                name={user.name}
+                avatar={user.avatarUrl}
+                storiesCount={user.articlesAmount || 0}
+              />
+              <ProfileTabs />
+            </>
+          )}
+        </section>
         <section className={css.storiesSection}>
           {isMyStories ? my : saved}
         </section>
--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,22 +1,3 @@
-'use client';
-
-import { useAuthStore } from '@/lib/store/useAuthStore';
-import TravellerInfo from '@/components/ui/TravellerInfo/TravellerInfo';
-import ProfileTabs from '@/components/ui/ProfileTabs/ProfileTabs';
-
 export default function ProfilePage() {
-  const user = useAuthStore((state) => state.user);
-
-  if (!user) return null;
-
-  return (
-    <>
-      <TravellerInfo
-        name={user.name}
-        avatar={user.avatarUrl}
-        storiesCount={user.articlesAmount || 0}
-      />
-      <ProfileTabs />
-    </>
-  );
+  return null;
 }
