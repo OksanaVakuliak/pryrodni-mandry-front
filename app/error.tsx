b83a065a10cbc51ad99ec446d951fdf585b8953c
Oksana Vakuliak <oksanabagrova19@gmail.com>
2026-04-07 15:11:05 +0200

    fix: refactor error and not found handling to use dedicated components

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import ErrorClient from '@/components/Errors/ErrorClient';
+
 export default function Error({
   error,
   reset,
@@ -7,22 +9,5 @@ export default function Error({
   error: unknown;
   reset: () => void;
 }) {
-  const message =
-    error instanceof Error
-      ? (error as Error).message
-      : 'Сталася помилка. Спробуйте ще раз.';
-
-  return (
-    <div className="problem-wrappererror">
-      <div className="problem-inner">
-        <h1 className="problem-title">Щось пішло не так</h1>
-
-        <p className="problem-text">{message}</p>
-
-        <button onClick={reset} className="problem-buttonerror">
-          Спробувати ще раз
-        </button>
-      </div>
-    </div>
-  );
+  return <ErrorClient error={error} reset={reset} />;
 }
